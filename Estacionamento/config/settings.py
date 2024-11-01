2e9974c3313c6aeb615b6cd2d341c7707849d250
Alan Givaldo <143469032+Alanxx69@users.noreply.github.com>
2024-11-01 18:23:00 -0300

    Delete Estacionamento directory

--- Estacionamento/config/settings.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    'host': 'database-univesp.chakokagese6.us-east-1.rds.amazonaws.com',
-    'port': '5432',
-    'database': 'estacionamento',
-    'user': 'pi_univesp',
-    'password': 'PIunivesp'
-}
